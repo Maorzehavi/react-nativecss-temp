ba181963e04741baf38ade2081be2a45f960b806
Maorz <maorzehavi@outlook.com>
2025-05-18 23:23:49 +0300

    init nativewind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View className={'flex-1 justify-between items-center'}>
+            <Text className={'text-3xl text-sky-500 font-bold'}>Welcome.</Text>
+        </View>
+    );
+}
